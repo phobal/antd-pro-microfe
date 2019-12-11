26815224e45b52196b289ae096639c4db835f9df
Phobal <phobal@126.com>
2019-12-11 15:04:52 +0800

    add issue demo

--- app1/src/pages/Welcome.tsx
+++ app1/src/pages/Welcome.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Card } from 'antd';
+import { Card, Icon } from 'antd';
 import { connect } from 'dva';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { CurrentUser } from '@/models/user';
 import { ConnectState } from '@/models/connect';
+import styles from './welcome.less'
 
 const Welcome = (props: CurrentUser): React.ReactNode => (
   <PageHeaderWrapper>
-    <Card>{`嗨，大家好，我叫 ${props.name}, 这是基于 and design pro 创建的项目 APP1`}</Card>
+    <Card>
+      <Icon className={styles.upgradeIcon} type="arrow-up" />
+      <p>如果单独访问子项目 <a href="http://localhost:8011">http://localhost:8011</a>, 这里的箭头符号颜色是红色的</p>
+      <p>但是通过主系统 <a href="http://localhost:8010">http://localhost:8010</a> 进入到子系统这里箭头的颜色就是灰色的</p>
+    </Card>
   </PageHeaderWrapper>
 );
 
